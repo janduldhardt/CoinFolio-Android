c4c620c7ac1dfb86e24127848dc9575fc9ffc904
Jan Duldhardt <duldhardtjan@gmail.com>
2021-08-31 16:41:41 +0700

    Update MainActivity

--- app/src/main/java/com/example/coinfolio/MainActivity.kt
+++ app/src/main/java/com/example/coinfolio/MainActivity.kt
@@ -8,8 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         //Download and show coins
     }
 }
