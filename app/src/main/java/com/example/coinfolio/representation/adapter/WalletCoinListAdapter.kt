57143c4d1a65e75e51e7170f4236eae9d9475fe0
Jan Duldhardt <duldhardtjan@gmail.com>
2021-10-03 15:26:44 +0700

    Update UI with symbol for trend profit

--- app/src/main/java/com/example/coinfolio/representation/adapter/WalletCoinListAdapter.kt
+++ app/src/main/java/com/example/coinfolio/representation/adapter/WalletCoinListAdapter.kt
@@ -15,48 +15,63 @@ import com.example.coinfolio.utils.toPercentString
 import com.example.coinfolio.utils.toString8Decimals
 import java.math.BigDecimal
 
-class WalletCoinListAdapter (
+class WalletCoinListAdapter(
     private val coinList: List<UserCryptoCurrencyViewModel>,
     val listener: (UserCryptoCurrencyViewModel) -> Unit
-    ) : RecyclerView.Adapter<WalletCoinListAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<WalletCoinListAdapter.ViewHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WalletCoinListAdapter.ViewHolder =
-            ViewHolder(
-                LayoutInflater.from(parent.context).inflate(R.layout.item_wallet, parent, false)
-            )
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): WalletCoinListAdapter.ViewHolder =
+        ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_wallet, parent, false)
+        )
 
-        override fun onBindViewHolder(holder: WalletCoinListAdapter.ViewHolder, position: Int) {
-            holder.bindItems(coinList[position])
-        }
+    override fun onBindViewHolder(holder: WalletCoinListAdapter.ViewHolder, position: Int) {
+        holder.bindItems(coinList[position])
+    }
 
-        override fun getItemCount(): Int = coinList.size
+    override fun getItemCount(): Int = coinList.size
 
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            private val coinName: TextView = itemView.findViewById(R.id.textView_currency_name)
-            private val coinAmount: TextView = itemView.findViewById(R.id.textView_currency_amount)
-            private val coinPrice: TextView = itemView.findViewById(R.id.textView_currency_price)
-            private val coinLogo: ImageView = itemView.findViewById(R.id.imageView_currency_logo)
-            private val coinProfit: TextView = itemView.findViewById(R.id.textView_currency_price_profit)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val coinName: TextView = itemView.findViewById(R.id.textView_currency_name)
+        private val coinAmount: TextView = itemView.findViewById(R.id.textView_currency_amount)
+        private val coinPrice: TextView = itemView.findViewById(R.id.textView_currency_price)
+        private val coinLogo: ImageView = itemView.findViewById(R.id.imageView_currency_logo)
+        private val coinProfit: TextView =
+            itemView.findViewById(R.id.textView_currency_price_profit)
+        private val trendImageView: ImageView =
+            itemView.findViewById(R.id.imageView_wallet_item_profit_sign)
 
-            fun bindItems(coin: UserCryptoCurrencyViewModel) = with(itemView) {
-                coinName.text = "${coin.cryptoCurrencyName}"
-                coinPrice.text = "${coin.usdAmount.toCurrencyString()}"
-                coinAmount.text = "${coin.coinSymbol} ${coin.amount.toString8Decimals()}"
-                coinProfit.text = "${coin.getProfitInPercent().toPercentString()}"
-                val profit = coin.getProfitInPercent()
+        fun bindItems(coin: UserCryptoCurrencyViewModel) = with(itemView) {
+            coinName.text = "${coin.cryptoCurrencyName}"
+            coinPrice.text = "${coin.usdAmount.toCurrencyString()}"
+            coinAmount.text = "${coin.coinSymbol} ${coin.amount.toString8Decimals()}"
+            coinProfit.text = "${coin.getProfitInPercent().toPercentString()}"
+            val profit = coin.getProfitInPercent()
+            val isPos = profit >= BigDecimal(0)
 
-                val textColor = if (profit >= BigDecimal(0))  context.getColor((R.color.textColorGreen)) else context.getColor((R.color.textColorRed))
-                coinProfit.setTextColor(textColor)
+            val textColor =
+                if (isPos) context.getColor((R.color.textColorGreen)) else context.getColor((R.color.textColorRed))
+            coinProfit.setTextColor(textColor)
 
-                val circularProgressDrawable = CircularProgressDrawable(context)
-                circularProgressDrawable.strokeWidth = 5f
-                circularProgressDrawable.centerRadius = 30f
-                circularProgressDrawable.start()
-
-                Glide.with(itemView).load(coin.logoUrl).fitCenter()
-                    .placeholder(circularProgressDrawable).into(coinLogo)
-
-                setOnClickListener { listener(coin) }
+            (if (isPos) R.drawable.ic_baseline_trending_up_24 else R.drawable.ic_baseline_trending_down_24).also {
+                trendImageView.setImageResource(
+                    it
+                )
             }
+            trendImageView.setColorFilter(textColor)
+
+            val circularProgressDrawable = CircularProgressDrawable(context)
+            circularProgressDrawable.strokeWidth = 5f
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.start()
+
+            Glide.with(itemView).load(coin.logoUrl).fitCenter()
+                .placeholder(circularProgressDrawable).into(coinLogo)
+
+            setOnClickListener { listener(coin) }
         }
+    }
 }
